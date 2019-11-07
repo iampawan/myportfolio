c5b5bf83c2d78788f9d525fbe45af0844d8e358c
David Acevedo <davidfco1702@gmail.com>
2019-11-06 23:41:13 -0400

    resonsive_widget.dart logic was changed. Now if statement can return a Medium Screen Size.

--- lib/responsive_widget.dart
+++ lib/responsive_widget.dart
@@ -21,17 +21,17 @@ class ResponsiveWidget extends StatelessWidget {
   }
 
   static bool isMediumScreen(BuildContext context) {
-    return MediaQuery.of(context).size.width > 800 &&
-        MediaQuery.of(context).size.width < 1200;
+    return MediaQuery.of(context).size.width >= 800 &&
+            MediaQuery.of(context).size.width <= 1200;
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth > 800) {
+        if (constraints.maxWidth > 1200) {
           return largeScreen;
-        } else if (constraints.maxWidth < 1200 && constraints.maxWidth > 800) {
+        } else if (constraints.maxWidth <= 1200 && constraints.maxWidth >= 800) {
           return mediumScreen ?? largeScreen;
         } else {
           return smallScreen ?? largeScreen;
